d2b40a9ae74d80a1838dbb926b8cb6c75dca3fb3
Brandon B <87585179+Nodnarb321@users.noreply.github.com>
2023-05-22 14:55:11 -0400

    Started list of pokemon in scripts.js
    Started list of different pokemon in scripts.js. Object names are 'name', 'height', and 'type'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,1 @@
-alert('Hello World!');
-
-let favoriteFood = 'Wings';
-document.write(favoriteFood);
+let pokemonList = [{name: "Bulbasaur", height:0.7, type: ["grass", "poison"]},{name: "Squirtle", height:0.5, type: "water"},{name: "Wartortle", height:1, type: "water"},{name: "Blastoise", height:1.6, type: "water"}];
