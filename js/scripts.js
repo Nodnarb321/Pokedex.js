b9700745248cabffb0969de646f439304535bb63
Brandon B <87585179+Nodnarb321@users.noreply.github.com>
2023-05-22 14:59:44 -0400

    Updated for readability
    indented and updated code for better readability

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-let pokemonList = [{name: "Bulbasaur", height:0.7, type: ["grass", "poison"]},{name: "Squirtle", height:0.5, type: "water"},{name: "Wartortle", height:1, type: "water"},{name: "Blastoise", height:1.6, type: "water"}];
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height:0.7,
+    type: ["grass", "poison"]
+  },
+  {
+    name: "Squirtle",
+    height:0.5, 
+    type: "water"
+  },
+  {
+    name: "Wartortle", 
+    height:1, 
+    type: "water"
+  },
+  {
+    name: "Blastoise", 
+    height:1.6, 
+    type: "water"
+  }
+];
