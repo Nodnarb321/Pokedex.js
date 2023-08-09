6e983457c884e6dc64a4c421c2980e4d4bc929b4
Brandon B <87585179+Nodnarb321@users.noreply.github.com>
2023-08-09 14:48:23 -0400

    replaced old coding for DOM interaction
    replaced old function with DOM interaction

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,58 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
+
+  //adding each pokemon onto list
   function add(pokemon){
     pokemonList.push(pokemon);
   };
+
+  //grabs pokemon from repository
   function getAll(){
     return pokemonList;
   };
+
+  function addListItem(pokedexEntry){
+
+    //setting list to variable
+    let list = document.querySelector(".list");
+  
+    //creating list items
+    let listItem = document.createElement("li");
+  
+    //creating button for list items
+    let button = document. createElement("button");
+  
+    //adding names to pokemon inside buttons
+    button.innerText = `${pokedexEntry.name}`;
+  
+    //adding a class to the buttons
+    button.classList.add("pokemon-item");
+  
+    //adding click event listener to buttons
+    button.addEventListener('click', function(){
+      showListDetails(pokedexEntry);
+    });
+  
+    //appending button to list items
+    listItem.appendChild(button);
+  
+    //appending list items to list
+    list.appendChild(listItem);
+  
+    //passing pokemon object as parameter when called
+    function showListDetails(pokedexEntry){
+      console.log(pokedexEntry);
+    };
+  };
+
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem : addListItem
   };
 })();
+
+//adding pokemon as list items
 
 pokemonRepository.add({
   name: "Bulbasaur",
@@ -36,29 +78,7 @@ pokemonRepository.add({
   type: ["water"]
 });
 
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach(function(pokedexEntry){
-  //Add pokemon name
-  document.write(`<b>${pokedexEntry.name}</b><br>`);
-
-  //Pokemon Height
-  document.write(`Height: ${pokedexEntry.height}`);
-
-  //big pokemon
-   if (pokedexEntry.height > 1) {
-    document.write(` - Wow that's big!`);
-  };
-
-  document.write(`<br>`);
-
-  //Add Pokemon type
-  document.write(`Type: ${pokedexEntry.type[0]}`);
-
-  //Second type if needed
-  if (pokedexEntry.type[1] !== undefined) {
-    document.write(` and ${pokedexEntry.type[1]}`);
-  };
-
-  document.write(`<br><br>`);
+//adding pokemon to list
+pokemonRepository.getAll().forEach(function (pokedexEntry, index) {
+  pokemonRepository.addListItem(pokedexEntry, index);
 });
